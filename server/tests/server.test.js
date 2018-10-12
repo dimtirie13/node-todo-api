fc02b3f8f9103e288a6a05fd98ef76a6a3696ad7
dimitrie13 <dimitrie13@outlook.com>
2018-10-11 21:03:46 -0400

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,52 @@
+const expect = require('expect');
+const request = require('supertest');
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+// testing life cycle method to cath error before happening. once text added
+// the count will not be 0 it will start from one.
+// This will allow database to be empty by deleting everything
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+      request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+          // check to see of todo was added to mongodb
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should not create todo with invalid body data', (done) => {
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+    }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+      }).catch((e) => done(e));
+   });
+ });
+});
