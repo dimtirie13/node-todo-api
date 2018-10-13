604bd3b2731ff271c2c47f3ce3b32d997b8aea55
dimitrie13 <dimitrie13@outlook.com>
2018-10-13 01:11:20 -0400

    DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,7 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {ObjectID} = require('mongodb');
+const {User} = require('./../server/models/user');
+
+
+Todo.findOneAndRemove('5bc003d23cc44e36d057d918').then((todo) => console.log(todo));
